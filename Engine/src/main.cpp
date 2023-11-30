580c3b5e614e96aa7c745b7db3c6a468049331f3
yao.liu <top61ly@gmail.com>
2023-11-30 22:37:20 +0800

    basic vulkan create test

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -2,16 +2,47 @@
 #include <vector>
 #include <string>
 
+#include <vulkan/vulkan.h>
+
 using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vulkan Engine";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_2;
 
-    for (const string& word : msg)
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    
+    vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+    cout << extensionCount << " extensions supported" << endl;
+    for (size_t i = 0; i < extensionCount; i++)
     {
-        cout << word << " ";
+        cout << extensions[i].extensionName << endl;
     }
-    cout << endl;
+    vector<const char*> extensionsName = {
+        VK_KHR_SURFACE_EXTENSION_NAME,
+    };
+
+    createInfo.enabledExtensionCount = 0;
+    createInfo.ppEnabledExtensionNames = nullptr;
+
+    VkInstance instance;
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+    {
+        throw runtime_error("failed to create instance!");
+        return 1;
+    }
+
     return 0;
 }
